69ef617b46e4ff9102b80d189b2d45fa09c96215
Dawn Monroe <deetriplett@gmail.com>
2017-02-09 11:14:51 -0500

    added const posts

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@
 'use strict'; //Throws error, for testing
 
 const express = require('express');
+const posts = require('./mock/posts.json');
 const app     = express();
+
+//Require Blog Posts
+
 
 //Route for root of server using Location '/', request and response
 app.get('/', function(req, res){
   res.send("<h1>Thesis Project is up!</h1>");
-})
+});
+//Get Route for blog posts
+app.get('/blog', function(req, res){
+  res.send(posts);
+});
+
 //Set Development Server 3000 or 8080
 app.listen(8080, function(){
   console.log("Frontend Server is running on port 8080!")
