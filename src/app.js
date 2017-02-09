1dca0ec93d8c963bcf3ef868632ed25bc0bb7916
Dawn Monroe <deetriplett@gmail.com>
2017-02-09 10:26:24 -0500

    created const app, connected to server

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
 //Main File for Application//
 'use strict'; //Throws error, for testing
 
-const = require('express');
+const express = require('express');
+const app     = express();
+
+//Route for root of server using Location '/', request and response
+app.get('/', function(req, res){
+  res.send("Thesis Project is up!");
+})
+//Set Development Server 3000 or 8080
+app.listen(8080);
