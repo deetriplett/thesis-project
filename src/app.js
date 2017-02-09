104c921c0c0895a3fcf60f1f55a20997cb52a2aa
Dawn Monroe <deetriplett@gmail.com>
2017-02-09 15:24:51 -0500

    added res callback for blog posts

--- src/app.js
+++ src/app.js
@@ -2,21 +2,29 @@
 'use strict'; //Throws error, for testing
 
 const express = require('express');
+//Require Blog Posts
 const posts = require('./mock/posts.json');
 const app     = express();
 
-//Require Blog Posts
+
 
 
 //Route for root of server using Location '/', request and response
 app.get('/', function(req, res){
   res.send("<h1>Thesis Project is up!</h1>");
 });
-//Get Route for blog posts
-app.get('/blog', function(req, res){
-  res.send(posts);
-});
+  app.get('/blog/:title', function(req, res){
 
+    let title = req.params.title;
+    if(title === undefined) {
+      res.status(503);
+      res.send("This page is under construction");
+    } else {
+      let post = posts[title];
+      res.send(posts);
+    }
+
+});
 //Set Development Server 3000 or 8080
 app.listen(8080, function(){
   console.log("Frontend Server is running on port 8080!")
